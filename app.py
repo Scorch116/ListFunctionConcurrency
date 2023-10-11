8008cf09824b597a4240340f2ef7694d7aa81a4b
Scorch116 <e.dakin@hotmail.com>
2023-10-11 14:20:38 +0100

    Pushing the rest of application

--- app.py
+++ app.py
@@ -7,3 +7,46 @@ def lambda_handler(event, context):
 
     lambda_client = boto3.client('lambda', region_name=region)
     count = 0
+
+    # Create a paginator for the 'list_functions' operation.
+    paginator = lambda_client.get_paginator('list_functions')
+    lambda_iterator = paginator.paginate()
+
+    # For loop to iterate through listed functions and return function name and concurrency response
+    for items in lambda_iterator:
+        for function in items['Functions']:
+            function_name = function['FunctionName']
+
+            try:
+                response = lambda_client.get_function_concurrency(
+                    FunctionName=function_name
+                )
+
+                # If reserved concurrency is configured, print the details
+                if 'ReservedConcurrentExecutions' in response:
+                    count += 1
+                    reserved_concurrency = response['ReservedConcurrentExecutions']
+                    print(f"{count}. Function Name: {function_name}, Reserved Concurrency: {reserved_concurrency}")
+
+            except lambda_client.exceptions.ResourceNotFoundException:
+                # If the function is not found, skip it
+                pass
+            except Exception as e:
+                print(f"Error retrieving concurrency for {function_name}: {e}")
+
+            try:
+                response = lambda_client.list_provisioned_concurrency_configs(
+                    FunctionName=function_name
+                )
+
+                # If provisioned concurrency is configured, print the details
+                if 'ProvisionedConcurrencyConfigs' in response and response['ProvisionedConcurrencyConfigs']:
+                    provisioned_concurrency = response['ProvisionedConcurrencyConfigs'][0]['RequestedProvisionedConcurrentExecutions']
+                    count += 1
+                    print(f"{count}. Function Name: {function_name}, Provisioned Concurrency: {provisioned_concurrency}")
+
+            except lambda_client.exceptions.ResourceNotFoundException:
+                # If the function is not found, skip it
+                pass
+            except Exception as e:
+                print(f"Error retrieving provisioned concurrency for {function_name}: {e}")
